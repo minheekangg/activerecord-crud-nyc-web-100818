7b9eaac656e08e2e38d9905590f38601d25f4c44
Min Hee  Park <minheekangg@gmail.com>
2018-10-22 17:19:26 +0000

    automatically backed up by learn

--- db/migrate/20181022171721_create_movies.rb
+++ db/migrate/20181022171721_create_movies.rb
@@ -1,6 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-
-    
+    # add columns for title (string), release_date (integer), director (string), lead (string), and in_theaters (boolean).
+    add_column :movies, :title, :string
+    add_column :movies, :release_date, :integer
+    add_column :movies, :director, :string
+    add_column :movies, :lead, :string
+    add_column :movies, :in_theaters, :boolean
   end
 end
